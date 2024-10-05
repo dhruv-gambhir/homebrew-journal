ef26d45693f23f67647c013fce71167f40999507
Gambhir Dhruv <75164918+dhruv-gambhir@users.noreply.github.com>
2024-10-06 02:44:57 +0800

    Update journal.rb

--- Formula/journal.rb
+++ Formula/journal.rb
@@ -3,7 +3,7 @@ class Journal < Formula
   homepage "https://github.com/dhruv-gambhir/journal"
   url "https://github.com/dhruv-gambhir/journal/archive/v1.0.0.tar.gz"
   sha256 "42b4d368bc731b3aada0d953b3cbbb8bde19285e125452b01bbe1ed5129811db"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "main.sh"
