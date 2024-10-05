946e09428fd223c91063a11f1938ba4e77610c42
Gambhir Dhruv <75164918+dhruv-gambhir@users.noreply.github.com>
2024-10-06 00:49:50 +0800

    Update journal.rb

--- Formula/journal.rb
+++ Formula/journal.rb
@@ -1,7 +1,7 @@
 class Journal < Formula
   desc "A simple shell script terminal journal app"
-  homepage "https://github.com/dhruvgambhir/journal"
-  url "https://github.com/dhruvgambhir/journal/archive/v1.0.0.tar.gz"
+  homepage "https://github.com/dhruv-gambhir/journal"
+  url "https://github.com/dhruv-gambhir/journal/archive/v1.0.0.tar.gz"
   sha256 "363484e5d5ff7413bad6c42d1c42a489f596a28abb478bbe4341b017286f0e04"
   version "1.0.0"
 
